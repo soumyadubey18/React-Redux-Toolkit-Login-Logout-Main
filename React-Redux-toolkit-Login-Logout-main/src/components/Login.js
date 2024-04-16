92d8e160d0079f9b00cdcf0b7fbdd84e00229cb3
Soumya Dubey <88154123+soumyadubey18@users.noreply.github.com>
2024-04-16 23:32:03 +0530

    Add files via upload

---
+++ React-Redux-toolkit-Login-Logout-main/src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div >
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Soumya", age: 20, email: "soumya@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Login;
